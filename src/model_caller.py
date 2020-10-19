ea2b305fe5b66506ef94d2dc0a0479b9d8420911
Justin McGowen <justmcgowen@gmail.com>
2020-10-18 18:09:55 -0600

    predict outputs something with correct inputs, and hopefully outputs

--- src/model_caller.py
+++ src/model_caller.py
@@ -1,7 +1,6 @@
 import torch
 
-from src import data_utils
-import
+import data_utils
 import numpy as np
 
 def predict(model, poses_in, current):
@@ -20,25 +19,45 @@ def predict(model, poses_in, current):
     #This standardizes from mean and std of all the data. Might not be best to use on a single case
     data_mean, data_std, dim_to_ignore, dim_to_use = data_utils.normalization_stats(poses_in)
 
+    #data_utils expects a dict, nothing fancy, immediately deconvert it afterwards
     poses_dict = {0:poses_in}
-
-
-
-
+    #this also removes the 0's from the input data, reducing to 54 dimensions
     poses_in = data_utils.normalize_data( poses_dict, data_mean, data_std, dim_to_use, [], False )[0]
-
+    
     encoder_inputs = np.zeros((model.source_seq_len - 1, model.input_size), dtype=float)
     decoder_inputs = np.zeros((model.target_seq_len, model.input_size), dtype=float)
 
-    encoder_inputs[:, :] = poses_in[0:model.source_seq_len - 1, :]
+    #source_seq_len - 1 preceding frames to current frame (ex: len = 3, current = 10, gives 8, 9, leaves 10 for decoder
+    encoder_inputs[:, :] = poses_in[current-model.source_seq_len+1:current, :]
+    #Only need to feed in the current pose to start the feedback loop
+    #For some reason, decoder_inputs doesn't usually do anything
+    #for the future recurrent nodes unless you just feed ground truth to the network.
+    decoder_inputs[0, :] = poses_in[current, :]
+
+    encoder_inputs = torch.from_numpy(encoder_inputs).float()
+    decoder_inputs = torch.from_numpy(decoder_inputs).float()
+    
+    encoder_inputs = torch.unsqueeze(encoder_inputs, 0)
+    decoder_inputs = torch.unsqueeze(decoder_inputs, 0)
+
+    encoder_inputs = encoder_inputs.cuda()
+    decoder_inputs = decoder_inputs.cuda()
 
     out = model(encoder_inputs, decoder_inputs)
+    
+    #probably the slow step honestly... try gpu or batch?
+    out = out.cpu().data.numpy()
+    out = out.transpose([1,0,2])
+
+    out_reverted = data_utils.revert_output_format(out, data_mean, data_std, dim_to_ignore, [], False)
+    #Looks like this is built to handle batches too...
+    return out_reverted[0].reshape(out_reverted[0].shape[0], 33, 3)
 
 #Hardcoded as a test
 if __name__ == "__main__":
     filename  = "./data/h3.6m/dataset/S1/walking_1.txt"
-    model_location = "./experiments/walking/out_25/iterations_10000/tied/sampling_based/one_hot/depth_1/size_1024/lr_0.005/residual_vel/model_10000"
-    import pdb; pdb.set_trace()
+    model_location = "./experiments/walking/out_25/iterations_10000/tied/sampling_based/omit_one_hot/depth_1/size_1024/lr_0.005/residual_vel/model_10000"
     action_sequence = data_utils.readCSVasFloat(filename)
     model = torch.load(model_location)
-    predict(model, action_sequence, 30)
+    prediction = predict(model, action_sequence, 70)
+    pass
